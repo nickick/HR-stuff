9cf3ae54a97001ec27318e1e94ad25ef5c68e5b3
Nick Hong <hong.nick@gmail.com>
2014-05-19 21:26:45 -0700

    in progress

--- MakingChange/src/change.js
+++ MakingChange/src/change.js
@@ -16,5 +16,51 @@
 var coinValues = [10000, 5000, 2000, 1000, 500, 100, 50, 25, 10, 5, 1];
 
 var makeChange = function(amount) {
-  return 4;
+  var newCoinValues = filter(coinValues, amount);
+  var result = [];
+
+  for (var i = newCoinValues.length - 1; i >= 0; i--) {
+    var division, modulo, value, obj;
+    value = newCoinValues[i];
+    division = amount / value;
+    modulo = amount % value;
+
+    obj = {
+      newCoinValues[i]..toString() : division
+    }
+    result.push(obj);
+    if (modulo == 0)
+
+    makeChange
+  }
 };
+
+var makeChangeSub = function(array, subamount) {
+  // .pop() takes last out of array and modifies array in place
+  if (array.length == 0 || subamount == 0) {
+    break;
+  }
+  var divisor = array.pop();
+  var division = subamount / divisor;
+  for (var i = 0; i <= division; i++) {
+    newSubamount = subamount % (i * divisor);
+    makeChangeSub(array, newSubamount);
+  }
+}
+
+function filter(coinValues, amount) {
+  var values = [];
+  for (var i = coinValues.length - 1; i >= 0; i--) {
+    var value = coinValues[i];
+    if (value <= amount) {
+      values.push(value);
+    } else {
+      break;
+    }
+  }
+
+  return values;
+}
+
+// makeChange(1)
+// => [{ '1' : 1 }]
